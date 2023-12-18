33ce5999109846c8f9e6514086ea906fd80ac006
Caitlin Allen Akselrud NOAA <caitlin.allen_akselrud@noaa.gov>
2023-12-18 13:22:57 -0800

    build: model selection *added Matt's code to get diagnostics as a function * added model selection section to call model results and produce diagnostic csv

--- R_scripts/main_code.R
+++ R_scripts/main_code.R
@@ -241,6 +241,118 @@ margAIC <- function(optim_model) {
   return(margAIC)
 }
 
+get_diagnostics <- function(fleet, fleet_name, models)
+{
+  # Generate full factorial design for naming models
+  map_factorial <- tidyr::crossing(rho_y = 0:1, rho_c = 0:1, rho_a = 0:1) %>% 
+    data.frame()
+  
+  # Model Checking ----------------------------------------------------------
+  
+  AIC_models <- vector() # store aic
+  nlminb_conv <- vector() # store nlminb convergence diagnostic
+  grad_conv <- vector() # store maximum gradient
+  grad_name_conv <- vector() # store parameter with maximum gradient
+  hess_conv <- vector() # whether or not Hessian is positive definite 
+  par_values <- data.frame(rho_y = NA, rho_a = NA, 
+                           rho_c = NA, log_sigma2 = NA) # parameter values
+  par_sd_values <- data.frame(rho_y_sd = NA, rho_a_sd = NA, 
+                              rho_c_sd = NA,  log_sigma2_sd = NA) # se values
+  
+  for(i in 1:length(models)) {
+    
+    # Convergence diagnostics
+    AIC_models[i] <- margAIC(models[[i]]$optim) # get aic
+    nlminb_conv[i] <- models[[i]]$optim$convergence # get nlminb convergence
+    grad_conv[i] <- max(abs(models[[i]]$sd_rep$gradient.fixed)) # max gradient
+    # Get max gradient parameter name
+    grad_name_conv[i] <- names(models[[i]]$sd_rep$par.fixed)[which.max(abs(models[[i]]$sd_rep$gradient.fixed))]
+    hess_conv[i] <- models[[i]]$sd_rep$pdHess # whether or not this is pd HESS
+    
+    # Grab parameter values here and store
+    par_values[i,] <- models[[i]]$sd_rep$par.fixed[match(colnames(par_values),
+                                                         names(models[[i]]$sd_rep$par.fixed))]
+    
+    # Get parameter std values here and store
+    par_sd_values[i,] <- sqrt(
+      diag(models[[i]]$sd_rep$cov.fixed)[match(colnames(par_values),
+                                               names(diag(models[[i]]$sd_rep$cov.fixed)))]
+    )
+    
+    if(i == length(models)) {
+      model_diag <- data.frame(AIC = AIC_models, nlminb_conv = nlminb_conv,
+                               max_grad_name = grad_name_conv,
+                               max_grad = grad_conv, pd_Hess = hess_conv)
+      
+      # Bind parameter values and sd together
+      model_diag <- cbind(model_diag, par_values, par_sd_values)
+    } # when done w/ evaluating all models
+    
+  } # end i loop
+  
+  
+  # Munge into plot format --------------------------------------------------
+  
+  # Create model names to differentiate models
+  model_names <- map_factorial %>% 
+    mutate(rho_y_lab = case_when(rho_y == 1 ~ "y"),
+           rho_a_lab = case_when(rho_a == 1 ~ "a"),
+           rho_c_lab = case_when(rho_c == 1 ~ "c")) %>% 
+    dplyr::select(rho_y_lab, rho_a_lab, rho_c_lab) %>% 
+    dplyr::rowwise() %>% 
+    tidyr::unite('model', na.rm = TRUE)
+  
+  # Input model names above into model_diag df
+  model_diag$model <- model_names$model
+  # No correlation parameters estimated
+  model_diag$model[model_diag$model == ""] <- "None"
+  
+  model_diag %<>% dplyr::filter(!is.na(AIC))
+  
+  # Pivot this dataframe longer 
+  # Parmeter MLEs here only (doing ses and binding in 2 steps)
+  model_pars_long <- model_diag %>% 
+    # mutate(
+    #   rho_a_trans = 2 / (1 + exp(-2 * rho_a)) - 1,
+    #   rho_y_trans = 2 / (1 + exp(-2 * rho_y)) - 1,
+    #   rho_c_trans = 2 / (1 + exp(-2 * rho_c)) - 1
+    # ) %>% # transform rhos
+    dplyr::select(-rho_a_sd, -rho_y_sd, -rho_c_sd, -log_sigma2_sd) %>% 
+    pivot_longer(cols = c(rho_a, rho_c, rho_y, log_sigma2), 
+                 names_to = "parameters",  values_to = "mle_val")
+  
+  # Get SE values now
+  model_se_long <- model_diag %>% 
+    dplyr::select(rho_a_sd, rho_y_sd, rho_c_sd, log_sigma2_sd) %>% 
+    pivot_longer(cols = everything(),names_to = "sd",  values_to = "sd_val")
+  
+  # Now bind, these two together
+  model_diag_long <- cbind(model_pars_long, model_se_long)
+  
+  # Create lwr and upr confidence intervals here
+  model_diag_long <- model_diag_long %>% 
+    mutate(lwr_95 = ifelse(parameters == "log_sigma2", exp(mle_val - (1.96 * sd_val)),
+                           mle_val - (1.96 * sd_val)),
+           upr_95 = ifelse(parameters == "log_sigma2", exp(mle_val + (1.96 * sd_val)),
+                           mle_val + (1.96 * sd_val)),
+           mle_val = ifelse(parameters == "log_sigma2", exp(mle_val), mle_val),
+           dAIC = min(AIC) - AIC)
+  
+  # Calcualte wAIC
+  wAIC <- model_diag_long %>% 
+    group_by(model) %>% 
+    summarize(dAIC = abs(mean(dAIC))) %>% 
+    ungroup() %>% 
+    mutate(wAIC = exp(-0.5 * dAIC) / sum(exp(-0.5 * dAIC)))
+  
+  # Left join wAIC
+  model_diag_long <- model_diag_long %>% 
+    left_join(wAIC %>% dplyr::select(wAIC, model), by = c("model"))
+  
+  # Output to csv
+  write.csv(model_diag_long, here::here("output", paste0(fleet_name, "_model_diag_vals.csv")))
+}
+
 # conditional model -------------------------------------------------------
 
 # * choose factorial design -----------------------------------------------
@@ -337,5 +449,25 @@ for(i in 1:n_fleets)
 
 
 # model selection ---------------------------------------------------------
+
+
+load(here("output", "2020_sardine_fleet1_cond_var_waa_models.RData"))
+get_diagnostics(fleet = 1, 
+                fleet_name = "fleet1", 
+                models = models_cond)
+
+
+load(here("output", "2020_sardine_fleet2_cond_var_waa_models.RData"))
+get_diagnostics(fleet = 2, 
+                fleet_name = "fleet2", 
+                models = models_cond)
+
+load(here("output", "2020_sardine_fleet3_cond_var_waa_models.RData"))
+get_diagnostics(fleet = 3, 
+                fleet_name = "fleet3", 
+                models = models_cond)
+
+
+# select waa matrix -------------------------------------------------------
 
 
